5a0af2fca42626de159e9b8a692fa01efe4456ee
Felix Sun <sunfmin@gmail.com>
2013-04-17 17:03:52 +0800

    initialize

---
+++ QortexCocoaSDKTestSuite/PublicServiceTest.m
@@ -0,0 +1,37 @@
+//
+//  PublicServiceTest.m
+//  PublicServiceTest
+//
+//  Created by Felix Sun on 04/17/13.
+//  Copyright (c) 2013 The Plant. All rights reserved.
+//
+
+#import "PublicServiceTest.h"
+#import "Qortexapi.h"
+
+@implementation PublicServiceTest
+
+- (void)setUp {
+	[super setUp];
+	[[Qortexapi get] setBaseURL:@"http://localhost:5000/api"];
+	[[Qortexapi get] setVerbose:YES];
+	[self setPublicService:[PublicService alloc]];
+}
+
+- (void)tearDown {
+
+	[super tearDown];
+}
+
+- (void)testGetSession {
+	PublicServiceGetSessionResults *r = [_publicService GetSession:@"sunfmin@gmail.com" password:@"nopassword"];
+	STAssertTrue([r.Session length] > 10, @"session is nil for correct password: %@", r.Session);
+}
+
+- (void)testGetSessionWithWrongLogin {
+	PublicServiceGetSessionResults *r = [_publicService GetSession:@"user@gmail.com" password:@"wrongpassword"];
+	STAssertTrue([r.Session isEqualToString:@""], @"session not nil for wrong password %@", r.Session);
+	STAssertTrue(r.Err != nil, @"err is nil");
+}
+
+@end
