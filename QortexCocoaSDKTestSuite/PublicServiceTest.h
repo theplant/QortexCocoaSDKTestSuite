86f200a802db5f3ad653303cf97b9f80e44a8009
Felix Sun <sunfmin@gmail.com>
2013-04-22 16:54:43 +0800

    Revert "sanze add new project"
    This reverts commit 1382c7c3b8de1203481089804e5158f49dcecce4.

---
+++ QortexCocoaSDKTestSuite/PublicServiceTest.h
@@ -0,0 +1,15 @@
+//
+//  PublicServiceTest.h
+//  PublicServiceTest
+//
+//  Created by Felix Sun on 04/17/13.
+//  Copyright (c) 2013 The Plant. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "Qortexapi.h"
+
+@interface PublicServiceTest : SenTestCase
+@property PublicService * publicService;
+
+@end
